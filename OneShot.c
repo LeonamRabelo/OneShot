e397cbe0a4acc9461e94850d6e9ee5be6412d732
Leonam Rabelo <rabeloleonam@gmail.com>
2025-02-04 10:39:13 -0300

    Adicionando comentarios ao codigo

--- OneShot.c
+++ OneShot.c
@@ -1,14 +1,18 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
+#include <stdio.h>         //Biblioteca padrão do C.
+#include "pico/stdlib.h"  //Biblioteca padrão do Raspberry Pi Pico para controle de GPIO, temporização e comunicação serial.
+#include "pico/time.h"   //Biblioteca para gerenciamento de temporizadores e alarmes.
 
+//Definiçãos de pinos
 #define PIN_BUTTON 5
 #define PIN_LED_GREEN 11
 #define PIN_LED_BLUE 12
 #define PIN_LED_RED 13
 
+//Variáveis globais
 volatile bool fila_leds_ativa = false;
 volatile int estado = 0;
 
+//Função para inicializar os pinos dos leds e botão, além da comunicação serial caso necessário
 void inicializaPinos(){
     stdio_init_all();
 
@@ -31,41 +35,44 @@ void inicializaPinos(){
     gpio_put(PIN_LED_RED, 0);
 }
 
+//Função de callback para desligar o LED após o tempo programado.
 int64_t turn_off_callback(alarm_id_t id, void *user_data){
-if(estado == 0){
-    gpio_put(PIN_LED_BLUE, 0);
-    estado++;
-    add_alarm_in_ms(3000, turn_off_callback, NULL, false);  //Agenda nova chamada para desligar o segundo led
-}else if(estado == 1){
-    gpio_put(PIN_LED_RED, 0);
-    estado++;
-    add_alarm_in_ms(3000, turn_off_callback, NULL, false);
-}else if(estado == 2){
-    gpio_put(PIN_LED_GREEN, 0);
-    fila_leds_ativa = false;
-    estado = 0;
-}
+    if(estado == 0){
+        gpio_put(PIN_LED_BLUE, 0);  //Desliga primeiro led
+        estado++;       //Incrementa valor do estado para proxima chamada
+        add_alarm_in_ms(3000, turn_off_callback, NULL, false);  //Agenda nova chamada para desligar o segundo led
+    }else if(estado == 1){
+        gpio_put(PIN_LED_RED, 0);   //Desliga segundo led
+        estado++;       //Incrementa valor do estado para proxima chamada
+        add_alarm_in_ms(3000, turn_off_callback, NULL, false);  //Agenda nova chamada para desligar o terceiro led
+    }else if(estado == 2){
+        gpio_put(PIN_LED_GREEN, 0);  //Desliga terceiro e último led
+        fila_leds_ativa = false;    //Após todos leds desligados, define a fila de leds ativa como falsa
+        estado = 0;     //Define ao valor inicial
+    }
 return 0;
 }
 
+//Função para iniciar a fila de leds e agendar a primeira chamada
 void iniciar_fila_leds(){
 if(fila_leds_ativa){
-    return;  //Evita múltiplos acionamentos
+    return;  //Evita múltiplos acionamentos em caso dos leds já estarem ligados
 }
 
-fila_leds_ativa = true;
+fila_leds_ativa = true; //Define a fila de leds ativa como verdadeira
+//Ativa todos leds
 gpio_put(PIN_LED_BLUE, 1);
 gpio_put(PIN_LED_RED, 1);
 gpio_put(PIN_LED_GREEN, 1);
 
-//Primeira chamada para desligar o primeiro led
+//Primeiro agendamento para desligar o primeiro led
 add_alarm_in_ms(3000, turn_off_callback, NULL, false);
 }
 
-bool debounce_button() {
-static uint32_t last_press_time = 0;
-uint32_t current_time = to_ms_since_boot(get_absolute_time());
-    if(current_time - last_press_time > 200){ // Debounce de 200ms
+bool debounce_button(){
+static uint32_t last_press_time = 0;    //Ultimo evento em ms
+uint32_t current_time = to_us_since_boot(get_absolute_time());  //Obtém o tempo atual em microssegundos
+    if(current_time - last_press_time > 200000){ //Debounce de 200ms
         last_press_time = current_time;
         return true;
     }
@@ -73,13 +80,15 @@ return false;
 }
 
 int main(){
-inicializaPinos();
+inicializaPinos();  //Inicializa os pinos e comunicação serial, se necessário
 
 while(true){
-    if(gpio_get(PIN_BUTTON)==0 && debounce_button()){
-        iniciar_fila_leds();
+    if(gpio_get(PIN_BUTTON)==0 && debounce_button()){   //Verifica se o botão foi pressionado com debounce
+        iniciar_fila_leds();    //Inicia a fila de leds
     }
 
+    // Introduz uma pequena pausa de 10 ms para reduzir o uso da CPU.
+    // Isso evita que o loop seja executado muito rapidamente e consuma recursos desnecessários.
     sleep_ms(10);
 }
 
